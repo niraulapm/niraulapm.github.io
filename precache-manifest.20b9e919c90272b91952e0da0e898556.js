2bc8455374482a1d30a80340ffeb8755ce0d0c2f
niraulapm <prashantniraula@gmail.com>
2020-03-10 00:55:57 -0400

    Updates

---
+++ precache-manifest.20b9e919c90272b91952e0da0e898556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c7cdef319fc2a1ff0b2138d7b02cfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f6d954b0209f9bb693d",
+    "url": "/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "c80191c00bf638bbf074",
+    "url": "/static/js/2.3f78d51d.chunk.js"
+  },
+  {
+    "revision": "7bc9b478038c6bedf8340d67bd512bd3",
+    "url": "/static/js/2.3f78d51d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6d954b0209f9bb693d",
+    "url": "/static/js/main.c3cb0ee4.chunk.js"
+  },
+  {
+    "revision": "e45e20948a247110e75b",
+    "url": "/static/js/runtime-main.46b10d78.js"
+  }
+]);
