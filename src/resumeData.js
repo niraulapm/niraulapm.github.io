daa1e6ed81ebf19bd50ddfafb4ea7364408f2eeb
niraulapm <prashantniraula@gmail.com>
2020-03-10 00:27:14 -0400

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,119 @@
+let resumeData = {
+    "imagebaseurl":"https://niraulapm.github.io/",
+    "name": "Prashanta M. Niraula",
+    "role": "Data science and machine learning enthusiast",
+    "emailID":"prashantniraula@gmail.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I'm really passionate about things I enjoy. I like to try new things and try to keep growing.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/prashanta-niraula-b1ba3969/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/niraulapm",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/prashantniraula",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a PhD candidate at Department of Physics, Western Michigan University. I am a self taught programmer, currently diving deeper into Machine Learning.",
+    "address":"Kalamazoo,MI,USA",
+    "website":"https://niraulapm.github.io",
+    "education":[
+      {
+        "UniversityName":"Western Michigan University",
+        "specialization":"Physics, PhD candidate",
+        "MonthOfPassing":"Graduating in : Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Dissertation title : Enhancement of critical current density in h\
+        igh temperature superconductor by ion beam irradiation."
+      },
+      {
+        "UniversityName":"Tribhuvan University",
+        "specialization":"M.Sc. in Physics",
+        "MonthOfPassing":"Graduated in : ",
+        "YearOfPassing":"2012",
+        "Achievements":"Elective courses: Solid State Physics, Atmospheric Physics"
+      },
+      {
+        "UniversityName":"Tribhuvan University",
+        "specialization":"B.Sc. in Physics",
+        "MonthOfPassing":"Graduated in : ",
+        "YearOfPassing":"2009",
+        "Achievements":"Elective courses: Research Methodology, Bio-statistics"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Western Michigan University",
+        "specialization":"Research Assistant",
+        "MonthOfLeaving":"Since : Jan",
+        "YearOfLeaving":"Sept 2015",
+        "Achievements":"Carried out mathematical calculation,computer simulation,acquiring data, analysing and\
+        plotting data, fitting the experimental data with the theoretical model."
+      },
+      {
+        "CompanyName":"Western Michigan University",
+        "specialization":"Teaching Assistant",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"Sept 2013",
+        "Achievements":"Graded assigned homework, quizzes, and exams of undergraduate physics courses"
+      }
+    ],
+    "skillsDescription":"Programming languages and tools",
+    "skills":[
+      {
+        "skillname":"Programming languages : Python, SQL, Latex, HTML, CSS, Javascript"
+      },
+      {
+        "skillname":"Tools : Numpy, Scipy, Pandas, scikit-learn, keras, tensorflow, pytorch,\
+        matplotlib, seaborn, plotly, django, flask"
+      },
+      {
+        "skillname":"Other : Git, Github, Unix system"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"SQUID data analysis",
+        "description":"In this project pandas,matplotlib and scipy libraries\
+        are used for reading .dat file, and\
+        fitting the data and doing necessary extrapolation.",
+        "imgurl":"images/portfolio/Squid.png",
+        "url":"https://github.com/niraulapm/squid_data_analysis"
+      },
+      {
+        "name":"Amazon item rating",
+        "description":"In this project plotly library is used to visualize the\
+        product rating through interactive box and bar plots.",
+        "imgurl":"images/portfolio/amazon.jpeg",
+        "url":"https://github.com/niraulapm/amazon_rating_visualization"
+      },
+      {
+        "name":"Github webpage",
+        "description":"In this project I have created and deployed web page using HTML,\
+        CSS, Javascript, git and github",  
+        "imgurl":"images/portfolio/github.png",
+        "url":"https://niraulapm.github.io/"
+      }
+
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
