4eb10f829bac9d9121b32d04f539cceedb379278
niraulapm <prashantniraula@gmail.com>
2023-09-05 20:15:12 -0400

    website updated

---
+++ precache-manifest.5f8eb7c0b1d09893ee8d1bcc45ea2f05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48e097a8b1e2977bc4c0",
+    "url": "/static/css/main.c5e31b2d.chunk.css"
+  },
+  {
+    "revision": "48e097a8b1e2977bc4c0",
+    "url": "/static/js/main.48e097a8.chunk.js"
+  },
+  {
+    "revision": "6778b76ec33dfa7d81c1",
+    "url": "/static/js/1.6778b76e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9670fef1612d86635750dd838675c182",
+    "url": "/index.html"
+  }
+];
